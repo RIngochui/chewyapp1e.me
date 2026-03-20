308d349b1c2b52e1b1c78343f7de8d1570b66641
RIngochui <ringochewyis@gmail.com>
2026-03-20 18:22:13 -0400

    replace resume

--- src/windows/ResumeWindow.tsx
+++ src/windows/ResumeWindow.tsx
@@ -3,7 +3,7 @@ import { asset } from '../utils/asset';
 export default function ResumeWindow() {
   return (
     <iframe
-      src={asset('/Resume 2026 VERSION 2.docx.pdf')}
+      src={asset('/Resume 2026.docx.pdf')}
       style={{ width: '100%', height: '100%', border: 'none' }}
       title="Resume"
     />
